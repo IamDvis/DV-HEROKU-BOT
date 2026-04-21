79763b39d2c33b373724414a288175a621c12dec
NoobDuru <gokhaledurgesh4@gmail.com>
2026-04-21 22:42:13 +0530

    Restore smart upstream config integration

--- config.py
+++ config.py
@@ -16,6 +16,12 @@ BOT_TOKEN = getenv("BOT_TOKEN", None)
 
 HEROKU_API_KEY = getenv("HEROKU_API_KEY", "")
 
+UPSTREAM_REPO = getenv(
+    "UPSTREAM_REPO", "https://github.com/IamDvis/DV-MUSIC"
+)
+
+UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
+
 OWNER_ID = list(
     map(int, getenv("OWNER_ID", "1679112664").split())
 )
