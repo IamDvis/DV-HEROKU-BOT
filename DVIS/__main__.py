801ff4a20b383ed258e0abab8ee6c684386502ed
NoobDuru <gokhaledurgesh4@gmail.com>
2026-04-21 23:18:01 +0530

    Fix RuntimeError: No current event loop (Fix for Python 3.14)

--- DVIS/__main__.py
+++ DVIS/__main__.py
@@ -35,5 +35,6 @@ async def main():
     await app.stop()
 
 if __name__ == "__main__":
-    loop = asyncio.get_event_loop()
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
     loop.run_until_complete(main())
