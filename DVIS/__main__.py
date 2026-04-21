f0e1af36a6fe135ec4ca71176d04a18c2da9b3de
NoobDuru <gokhaledurgesh4@gmail.com>
2026-04-21 22:53:09 +0530

    Fix DeprecationWarning, add runtime.txt, setup.sh, and update requirements.txt

--- DVIS/__main__.py
+++ DVIS/__main__.py
@@ -28,5 +28,4 @@ async def main():
     await app.stop()
 
 if __name__ == "__main__":
-    loop = asyncio.get_event_loop_policy().get_event_loop()
-    loop.run_until_complete(main())
+    asyncio.run(main())
